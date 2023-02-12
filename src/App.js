1234f87e0ea808c3572080ed945ad1e56a0c106c
arrowls <babuyoff@gmail.com>
2023-02-12 13:04:58 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   const [hex, setHex] = useState('');
   const [rgb, setRgb] = useState('');
   const [copied, setCopied] = useState(false);
+  const [hexError, setHexError] = useState(false);
 
   function toRgbString() {
     setRgb('');
@@ -11,8 +12,10 @@ function App() {
     if (hex.length !== 6) return;
 
     const split = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-
-    if (!split) return;
+    if (!split) {
+      setHexError(true);
+      return;
+    }
     return `rgb(${
         parseInt(split[1], 16)
     }, ${
@@ -29,13 +32,14 @@ function App() {
   }
 
   useEffect(() => {
+    setHexError(false)
     setRgb(toRgbString());
   }, [hex])
 
   return (
     <div className="App" style={{ backgroundColor: rgb, }}>
       <div className="hex-convert">
-        <div className="hex-input-group">
+        <div className={`hex-input-group ${hexError && '_error'}`}>
           <input type="text"
                  className="hex-input"
                  value={hex}
